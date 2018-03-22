c93e2c53255c2e5e4ad1ae8b772e398d5b15dffa
zpjgit <zpj.love@foxmail.com>
2018-03-22 16:05:29 +0800

    [自定义异步广播]:自定义按键触发异步广播，当按键被按下后提示"received in MyBroadcastReceiver"

--- app/src/main/java/com/zpj/myapplication8/MainActivity.java
+++ app/src/main/java/com/zpj/myapplication8/MainActivity.java
@@ -1,14 +1,45 @@
 package com.zpj.myapplication8;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+    private MyBroadcastReceiver myBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//监视网络
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        myBroadcastReceiver = new MyBroadcastReceiver();
+//        registerReceiver(myBroadcastReceiver, intentFilter);
+
+//自定义的按键触发的广播
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.zpj.myapplication8.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(myBroadcastReceiver);
     }
 
 }
